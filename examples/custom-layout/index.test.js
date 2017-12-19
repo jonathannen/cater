059e4b286027b16dd3fcb1b3328e343232b60e43
Jon Williams <jon@jonathannen.com>
2017-12-19 00:21:14 -0800

    Renaming Part 1,512

--- examples/custom-layout/index.test.js
+++ examples/custom-layout/index.test.js
@@ -6,7 +6,7 @@ const hander = harness().testHandler();
 
 describe('Custom Layout Integration Test', () => {
 
-    test('should use the layout locally under server/Layout.js', async () => {
+    test('should use the layout locally under server/layout.js', async () => {
         const res = await request(hander).get('/');
         expect(res.statusCode).toBe(200);
         expect(res.text).toEqual(expect.stringContaining("This is a Custom Layout"));
--- packages/cater/test/util.default-app.js
+++ packages/cater/test/util.default-app.js
@@ -1,6 +1,6 @@
 // Copyright Jon Williams 2017. See LICENSE file.
 
-// Will throw an (intended) error defined in app/App. Needed as it's own 
+// Will throw an (intended) error defined in app/app. Needed as it's own 
 // file as a regular require skips the usual Babel import rules. So we 
 // require this file instead.
 import App from 'app/app';
