c97d7e4c4556838f215343315f0df37d12cc4480
Jon Williams <jon@jonathannen.com>
2017-12-23 13:34:19 -0700

    Fix up the build process

--- packages/cater/src/commands.js
+++ packages/cater/src/commands.js
@@ -1,16 +1,26 @@
 // Copyright Jon Williams 2017. See LICENSE file.
-// import del from "del";
+import fs from "fs-extra";
 import http from "http";
+import path from "path";
 import webpackBuild from "./webpack-build";
 
 // Returns a promise to a production build of the application.
 module.exports.runBuild = function() {
   this.build = true;
-  // The build directory gets nuked, then kick off webpack
-  // return del(this.buildPath).then(() => {
-  //   return webpackBuild(this);
-  // });
-  return Promise.resolve(true);
+
+  return fs
+    .remove(this.buildPath)
+    .then(() => {
+      if (this.devStaticPathExists) {
+        const prodStaticPath = path.join(this.buildPath, this.publicPath);
+        return fs.copy(this.devStaticPath, prodStaticPath);
+      }
+      return Promise.resolve(true);
+    })
+    .then(() => {
+      return webpackBuild(this);
+    });
+
 };
 
 // Runs the development server - that's a server with webpack in-memory
@@ -22,7 +32,7 @@ module.exports.runDev = function() {
 module.exports.runStart = function() {
   this.debug = false;
   return this.handler().then(h => this.runGenericServer(h));
-}
+};
 
 // Runs a generic server, given a http.Handler
 module.exports.runGenericServer = function(handler) {
