b19a216ed6fba0bb95f9fd64aaf5f56d7211c058
Jon Williams <jon@jonathannen.com>
2018-01-04 22:34:08 -0800

    Simplify the express example

--- examples/express-server/index.js
+++ examples/express-server/index.js
@@ -1,10 +1,23 @@
 // Copyright Jon Williams 2017-2018. See LICENSE file.
 const cater = require('cater');
-const server = require('./server');
+const express = require('express');
 
+// The Cater "app" is a parcel of all the different parts of a Cater-based
+// Application. The handler returns a Promise that can be used by most
+// http.Handler based frameworks. Following is an example using express.
 const app = cater();
 const port = app.httpPort || 3000;
 
+// An express server
+const server = express();
+
+// Add in some other routes, to make it interesting
+server.get('/express', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end('This page is rendered directly in Express.');
+});
+
+// Get the http.Handler from Cater and kick off the express server
 app
   .handler()
   .then((handler) => {
