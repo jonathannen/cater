8705775703b9c204e8d8d0906e094126670babef
Jon Williams <jon@jonathannen.com>
2018-01-06 11:37:50 -0800

    Extra comments and some debugging hooks

--- packages/cater-build/src/webpack-generator.js
+++ packages/cater-build/src/webpack-generator.js
@@ -118,7 +118,15 @@ function generate(context, side) {
   const resolve = {
     plugins: [
       {
+        // TODO: Not quite happy with how this turned out. We don't get to see
+        // that the import in the (say) assets/blah.scss file is actually
+        // 'assets/cat.png' rather than './assets/cat.png'. This covers the
+        // case, but not very neatly to my liking.
         apply: (resolver) => {
+          // Universal imports from the assets directory (say in SCSS files).
+          // Get clobbered into relative paths. Which then become
+          // <appRootPath>/assets/assets/cat.png rather than just assets. This
+          // Hook fixes that issue then it comes around.
           resolver.plugin('file', (ctx, callback) => {
             if (!ctx.path.includes('/assets/assets/') || fs.existsSync(ctx.path)) return callback();
             ctx.path = ctx.path.replace('/assets/assets/', '/assets/');
