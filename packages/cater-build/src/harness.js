4ad11693c9fd5116cdd5502d8d40442c1c6c3cce
Jon Williams <jon@jonathannen.com>
2018-03-01 20:03:23 +1100

    Tests actually passing

--- packages/cater-build/src/harness.js
+++ packages/cater-build/src/harness.js
@@ -1,7 +1,9 @@
 // Copyright Jon Williams 2017-2018. See LICENSE file.
 // Note ES2015 imports:
-import handlerCater from './handler-cater';
 import cater from '../index.js';
+import fs from 'fs';
+import handlerCater from './handler-cater';
+import path from 'path';
 import { Middleware } from 'cater-runtime';
 
 if (!global.INTERNAL_POLYFILL) {
@@ -13,11 +15,29 @@ export function testCater(options = {}) {
   return app;
 }
 
+function calculateTestRootDirectory() {
+  let dir = path.dirname(global.jasmine.testPath);
+
+  while (!fs.existsSync(path.join(dir, 'package.json'))) {
+    const next = path.join(dir, '..');
+    if (dir === next) return process.cwd(); // Hit the root directory
+    dir = next;
+  }
+
+  return dir;
+}
+
 /**
  * Returns a http.Handler that can be used in tests.
  */
 export function testHandler(appRootPath = null) {
-  const app = testCater({ appRootPath: appRootPath || process.cwd() });
+  let root = appRootPath;
+
+  if (!appRootPath && global.jasmine && global.jasmine.testPath) {
+    root = calculateTestRootDirectory();
+  }
+
+  const app = testCater({ appRootPath: root || process.cwd() });
 
   const handler = handlerCater(app);
   handler.load();
